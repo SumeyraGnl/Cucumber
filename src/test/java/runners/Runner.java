41b1cfde5fe9b6ca8a326636908c0d90987674e0
SumeyraGnl <h.berengunel@gmail.com>
2023-05-31 07:57:47 +0300

    31.05.2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,17 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
         plugin = {"html:target/cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
-                "junit:target/xml-report/cucumber.xml"},
-
+                "junit:target/xml-report/cucumber.xml"
+        },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@ss",
+        tags = "@ae",
 
         dryRun = false
 )
+
 
 public class Runner {
 
